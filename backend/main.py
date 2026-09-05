495de86ec195188063acb65487204a58ba4ce460
Fuad Hassen <fuadhas6634@gmail.com>
2026-09-05 17:46:12 +0530

    fix production cors

--- backend/main.py
+++ backend/main.py
@@ -121,7 +121,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "*"],
+    allow_origins=["http://localhost:5173", "https://echo-mail-pley.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
